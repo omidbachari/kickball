7601d1046003c240c5d92eca803f1861dba6ec65
Omid Omid <omidbachari@gmail.com>
2014-05-21 11:05:50 -0400

    Second commit

--- server.rb
+++ server.rb
@@ -2,24 +2,28 @@ require 'sinatra'
 require 'csv'
 require 'pry'
 
+
 all_player_data = []
 
 CSV.foreach('lackp_starting_rosters.csv', headers: true) do |player_data|
   all_player_data << player_data.to_hash
-
 end
 
 get '/' do
-  @all_player_data = all_player_data
   erb :index
 end
 
-get '/teams' do
+get '/teams/:team' do
+  @team = params[:team]
   @all_player_data = all_player_data
-  erb :teams
+    all_player_data.each do |player_data|
+      player_data[:team]
+    end
+erb :teams
 end
 
-get '/positions' do
+
+get '/positions/:position' do
   @all_player_data = all_player_data
   erb :positions
 end
